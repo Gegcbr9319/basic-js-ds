3a06554ff75de8e6ebc876eea262610e9397e454
Gegcbr9319 <gegcbr9319@gmail.com>
2022-05-02 23:57:49 +0300

    add: solution

--- src/stack.js
+++ src/stack.js
@@ -1,3 +1,4 @@
+//const createStatsCollector = require('mocha/lib/stats-collector');
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
@@ -13,19 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+  this.stack = [];}
+  push(element ) {
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop();
+    
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.stack[this.stack.length-1];
   }
 }
 
